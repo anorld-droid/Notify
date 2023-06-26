d85cdbd1b2d7273bf19f7e3a9faf0dc61c776146
anorld-droid <mulindipatrice00@gmail.com>
2023-06-26 17:59:47 +0300

    User-password file created.

---
+++ app/src/main/java/com/ratham/notify/data/Auth.kt
@@ -0,0 +1,9 @@
+package com.ratham.notify.data
+
+/**
+Created by Patrice Mulindi (email:mulindipatrice00@gmail.com) on 26/06/2023
+ **/
+
+val getAuthDetails = mapOf<String, String>(
+    "DN/000234/019" to "DN_A", "CCS/00008/019" to "student_A", "CCS/00007/019" to "student_B"
+)
